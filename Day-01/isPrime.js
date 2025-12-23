a983d5f12f34b4927c180a890176bbfceffd6139
Vikas Chamyal <72160525+vikaschamyal@users.noreply.github.com>
2025-12-24 00:03:40 +0530

    Add isPrime function to determine prime status
    Implement isPrime function to check for prime numbers.

---
+++ Day-01/isPrime.js
@@ -0,0 +1,12 @@
+function isPrime(n) {
+  if (n < 2) return false;
+  for (let i = 2; i * i <= n; i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+
+isPrime(4);
+isPrime(8);
+isPrime(1);
+isPrime(9);
