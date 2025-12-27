5a8d543c8402778b2610b007e1c6825ee350cf6d
Vikas Chamyal <72160525+vikaschamyal@users.noreply.github.com>
2025-12-28 00:41:20 +0530

    Create vowels.js

---
+++ Day-01/vowels.js
@@ -0,0 +1,10 @@
+// 4. Count vowels in string
+function countVowels(str) {
+  let count = 0;
+  let vowels = "aeiouAEIOU";
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) count++;
+  }
+  return count;
+}
+console.log(countVowels("Interview")); 
